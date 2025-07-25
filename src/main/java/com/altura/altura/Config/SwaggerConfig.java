82d9eb158c3dc0c9e8ba6b8a0e688ef839e3066b
Paradox281 <kharismatio220@gmail.com>
2025-07-25 17:23:47 +0700

    .

--- src/main/java/com/altura/altura/Config/SwaggerConfig.java
+++ src/main/java/com/altura/altura/Config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.altura.altura.Config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +13,7 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI alturaOpenAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://altura.up.railway.app/"))
                 .info(new Info()
                         .title("Altura API Documentation")
                         .description("API documentation for Altura Tourism Application")
